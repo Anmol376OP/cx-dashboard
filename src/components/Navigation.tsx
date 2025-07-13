b2f325ba4f36702f484f28cac2b40073e0486bed
Anmol376OP <anmolsrivastav7102@gmail.com>
2025-07-13 22:22:32 +0530

    remove unused variables

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -3,7 +3,7 @@
 import Link from "next/link"
 import { usePathname } from "next/navigation"
 import { cn } from "@/lib/utils"
-import { Activity, LayoutDashboard, Ticket, Wifi } from "lucide-react"
+import { Activity, LayoutDashboard, Ticket } from "lucide-react"
 
 const Navigation = () => {
   const pathname = usePathname()
